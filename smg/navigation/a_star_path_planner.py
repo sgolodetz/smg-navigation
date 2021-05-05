ff1df527b16767724942a8f39c67a9cc7988d505
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-05-05 17:29:33 +0100

    Move a lot of code into PathUtil.

--- smg/navigation/a_star_path_planner.py
+++ smg/navigation/a_star_path_planner.py
@@ -1,50 +1,22 @@
 import numpy as np
 
 from collections import defaultdict, deque
-from typing import Callable, Deque, Dict, List, Optional, Tuple
+from typing import Callable, Deque, Dict, List, Optional
 
-from smg.pyoctomap import OcTree, OcTreeNode, Vector3
+from smg.pyoctomap import OcTree, Vector3
 from smg.utility import PriorityQueue
+
+from .path_util import PathNode, PathUtil
 
 
 class AStarPathPlanner:
     """A path planner for Octomaps based on A*."""
 
-    # NESTED TYPES
-
-    Node = Tuple[int, int, int]
-
     # CONSTRUCTOR
 
-    def __init__(self, tree: OcTree, neighbours: Callable[[Node], List[Node]]):
-        self.__neighbours: Callable[[AStarPathPlanner.Node], List[AStarPathPlanner.Node]] = neighbours
+    def __init__(self, tree: OcTree, neighbours: Callable[[PathNode], List[PathNode]]):
+        self.__neighbours: Callable[[PathNode], List[PathNode]] = neighbours
         self.__tree: OcTree = tree
-
-    # PUBLIC STATIC METHODS
-
-    @staticmethod
-    def neighbours4(node: Node) -> List[Node]:
-        x, y, z = node
-        return [
-            (x, y, z - 1),
-            (x - 1, y, z),
-            (x + 1, y, z),
-            (x, y, z + 1)
-        ]
-
-    @staticmethod
-    def neighbours8(node: Node) -> List[Node]:
-        x, y, z = node
-        return [
-            (x - 1, y, z - 1),
-            (x, y, z - 1),
-            (x + 1, y, z - 1),
-            (x - 1, y, z),
-            (x + 1, y, z),
-            (x - 1, y, z + 1),
-            (x, y, z + 1),
-            (x + 1, y, z + 1)
-        ]
 
     # PUBLIC METHODS
 
@@ -56,29 +28,28 @@ class AStarPathPlanner:
         # 2) https://www.redblobgames.com/pathfinding/a-star/implementation.html
 
         if d is None:
-            d = self.__l2_distance
+            d = PathUtil.l2_distance
         if h is None:
-            h = self.__l2_distance
+            h = PathUtil.l2_distance
 
-        source_node: AStarPathPlanner.Node = self.__pos_to_node(Vector3(*source))
-        goal_node: AStarPathPlanner.Node = self.__pos_to_node(Vector3(*goal))
+        source_node: PathNode = PathUtil.pos_to_node(Vector3(*source), self.__tree)
+        goal_node: PathNode = PathUtil.pos_to_node(Vector3(*goal), self.__tree)
 
-        source_vpos: Vector3 = self.__node_to_vpos(source_node)
-        goal_vpos: Vector3 = self.__node_to_vpos(goal_node)
+        source_vpos: Vector3 = PathUtil.node_to_vpos(source_node, self.__tree)
+        goal_vpos: Vector3 = PathUtil.node_to_vpos(goal_node, self.__tree)
 
-        print(source, source_node, self.__occupancy_status(source_node))
-        print(goal, goal_node, self.__occupancy_status(goal_node))
+        print(source, source_node, PathUtil.occupancy_status(source_node, self.__tree))
+        print(goal, goal_node, PathUtil.occupancy_status(goal_node, self.__tree))
 
-        g_scores: Dict[AStarPathPlanner.Node, float] = defaultdict(lambda: np.infty)
+        g_scores: Dict[PathNode, float] = defaultdict(lambda: np.infty)
         g_scores[source_node] = 0.0
-        came_from: Dict[AStarPathPlanner.Node, Optional[AStarPathPlanner.Node]] = {source_node: None}
+        came_from: Dict[PathNode, Optional[PathNode]] = {source_node: None}
 
-        frontier: PriorityQueue[AStarPathPlanner.Node, float, type(None)] \
-            = PriorityQueue[AStarPathPlanner.Node, float, type(None)]()
+        frontier: PriorityQueue[PathNode, float, type(None)] = PriorityQueue[PathNode, float, type(None)]()
         frontier.insert(source_node, h(source_vpos, goal_vpos), None)
 
         while not frontier.empty():
-            current_node: AStarPathPlanner.Node = frontier.top().ident
+            current_node: PathNode = frontier.top().ident
             if current_node == goal_node:
                 path: Deque[np.ndarray] = self.__reconstruct_path(goal_node, came_from)
                 path.appendleft(source)
@@ -86,13 +57,13 @@ class AStarPathPlanner:
                 return np.vstack(path)
 
             frontier.pop()
-            current_vpos: Vector3 = self.__node_to_vpos(current_node)
+            current_vpos: Vector3 = PathUtil.node_to_vpos(current_node, self.__tree)
 
             for neighbour_node in self.__neighbours(current_node):
-                if self.__occupancy_status(neighbour_node) != "Free":
+                if PathUtil.occupancy_status(neighbour_node, self.__tree) != "Free":
                     continue
 
-                neighbour_vpos: Vector3 = self.__node_to_vpos(neighbour_node)
+                neighbour_vpos: Vector3 = PathUtil.node_to_vpos(neighbour_node, self.__tree)
                 tentative_cost: float = g_scores[current_node] + d(current_vpos, neighbour_vpos)
                 if tentative_cost < g_scores[neighbour_node]:
                     g_scores[neighbour_node] = tentative_cost
@@ -107,52 +78,11 @@ class AStarPathPlanner:
 
     # PRIVATE METHODS
 
-    def __node_to_vpos(self, node: Node) -> Vector3:
-        voxel_size: float = self.__tree.get_resolution()
-        half_voxel_size: float = voxel_size / 2.0
-        return Vector3(
-            node[0] * voxel_size + half_voxel_size,
-            node[1] * voxel_size + half_voxel_size,
-            node[2] * voxel_size + half_voxel_size
-        )
-
-    def __node_to_vpos_np(self, node: Node) -> np.ndarray:
-        return AStarPathPlanner.__to_numpy(self.__node_to_vpos(node))
-
-    def __occupancy_status(self, node: Node) -> str:
-        vpos: Vector3 = self.__node_to_vpos(node)
-        octree_node: Optional[OcTreeNode] = self.__tree.search(vpos)
-        if octree_node is None:
-            return "Unknown"
-        else:
-            occupied: bool = self.__tree.is_node_occupied(octree_node)
-            return "Occupied" if occupied else "Free"
-
-    def __pos_to_node(self, pos: Vector3) -> Node:
-        voxel_size: float = self.__tree.get_resolution()
-        return \
-            int(np.round(pos.x // voxel_size)), \
-            int(np.round(pos.y // voxel_size)), \
-            int(np.round(pos.z // voxel_size))
-
-    def __reconstruct_path(self, goal_node: Node, came_from: Dict[Node, Optional[Node]]) -> Deque[np.ndarray]:
+    def __reconstruct_path(self, goal_node: PathNode, came_from: Dict[PathNode, Optional[PathNode]]) \
+            -> Deque[np.ndarray]:
         path: Deque[np.ndarray] = deque()
-        current_node: Optional[AStarPathPlanner.Node] = goal_node
+        current_node: Optional[PathNode] = goal_node
         while current_node is not None:
-            path.appendleft(self.__node_to_vpos_np(current_node))
+            path.appendleft(PathUtil.node_to_vpos_np(current_node, self.__tree))
             current_node = came_from.get(current_node)
         return path
-
-    # PRIVATE STATIC METHODS
-
-    @staticmethod
-    def __from_numpy(v: np.ndarray) -> Vector3:
-        return Vector3(v[0], v[1], v[2])
-
-    @staticmethod
-    def __l2_distance(pos: Vector3, goal_pos: Vector3) -> float:
-        return np.linalg.norm(AStarPathPlanner.__to_numpy(pos) - AStarPathPlanner.__to_numpy(goal_pos))
-
-    @staticmethod
-    def __to_numpy(v: Vector3) -> np.ndarray:
-        return np.array([v.x, v.y, v.z])
--- smg/navigation/path_util.py
+++ smg/navigation/path_util.py
@@ -1,13 +1,26 @@
 import numpy as np
 
-from scipy.interpolate import CubicSpline
-from typing import List
+from smg.pyoctomap import OcTree, OcTreeNode, Vector3
 
+from scipy.interpolate import CubicSpline
+from typing import List, Optional, Tuple
+
+
+# HELPER TYPES
+
+PathNode = Tuple[int, int, int]
+
+
+# MAIN CLASS
 
 class PathUtil:
     """Utility functions related to paths."""
 
     # PUBLIC STATIC METHODS
+
+    @staticmethod
+    def from_numpy(v: np.ndarray) -> Vector3:
+        return Vector3(v[0], v[1], v[2])
 
     @staticmethod
     def interpolate(path: np.ndarray, *, smoothed_length: int = 100) -> np.ndarray:
@@ -16,7 +29,80 @@ class PathUtil:
         return cs(np.linspace(0, len(path) - 1, smoothed_length))
 
     @staticmethod
-    def pull_strings(path: np.ndarray) -> np.ndarray:
+    def is_traversible(path: np.ndarray, source: int, dest: int, tree: OcTree) -> bool:
+        source_node: PathNode = PathUtil.pos_to_node(PathUtil.from_numpy(path[source, :]), tree)
+        dest_node: PathNode = PathUtil.pos_to_node(PathUtil.from_numpy(path[dest, :]), tree)
+
+        print(source_node)
+        print(dest_node)
+
+        # TODO
+
+        return True
+
+    @staticmethod
+    def l2_distance(v1: Vector3, v2: Vector3) -> float:
+        return np.linalg.norm(PathUtil.to_numpy(v1) - PathUtil.to_numpy(v2))
+
+    @staticmethod
+    def neighbours4(node: PathNode) -> List[PathNode]:
+        x, y, z = node
+        return [
+            (x, y, z - 1),
+            (x - 1, y, z),
+            (x + 1, y, z),
+            (x, y, z + 1)
+        ]
+
+    @staticmethod
+    def neighbours8(node: PathNode) -> List[PathNode]:
+        x, y, z = node
+        return [
+            (x - 1, y, z - 1),
+            (x, y, z - 1),
+            (x + 1, y, z - 1),
+            (x - 1, y, z),
+            (x + 1, y, z),
+            (x - 1, y, z + 1),
+            (x, y, z + 1),
+            (x + 1, y, z + 1)
+        ]
+
+    @staticmethod
+    def node_to_vpos(node: PathNode, tree: OcTree) -> Vector3:
+        voxel_size: float = tree.get_resolution()
+        half_voxel_size: float = voxel_size / 2.0
+        return Vector3(
+            node[0] * voxel_size + half_voxel_size,
+            node[1] * voxel_size + half_voxel_size,
+            node[2] * voxel_size + half_voxel_size
+        )
+
+    @staticmethod
+    def node_to_vpos_np(node: PathNode, tree: OcTree) -> np.ndarray:
+        return PathUtil.to_numpy(PathUtil.node_to_vpos(node, tree))
+
+    @staticmethod
+    def occupancy_status(node: PathNode, tree: OcTree) -> str:
+        # FIXME: Use an enumeration for the return values.
+        vpos: Vector3 = PathUtil.node_to_vpos(node, tree)
+        octree_node: Optional[OcTreeNode] = tree.search(vpos)
+        if octree_node is None:
+            return "Unknown"
+        else:
+            occupied: bool = tree.is_node_occupied(octree_node)
+            return "Occupied" if occupied else "Free"
+
+    @staticmethod
+    def pos_to_node(pos: Vector3, tree: OcTree) -> PathNode:
+        voxel_size: float = tree.get_resolution()
+        return \
+            int(np.round(pos.x // voxel_size)), \
+            int(np.round(pos.y // voxel_size)), \
+            int(np.round(pos.z // voxel_size))
+
+    @staticmethod
+    def pull_strings(path: np.ndarray, tree: OcTree) -> np.ndarray:
         pulled_path: List[np.ndarray] = []
 
         i: int = 0
@@ -24,16 +110,13 @@ class PathUtil:
             pulled_path.append(path[i, :])
 
             j: int = i + 2
-            while j < len(path) and PathUtil.__is_traversible(path, i, j):
+            while j < len(path) and PathUtil.is_traversible(path, i, j, tree):
                 j += 1
 
             i = j - 1
 
         return np.vstack(pulled_path)
 
-    # PRIVATE STATIC METHODS
-
     @staticmethod
-    def __is_traversible(path: np.ndarray, i: int, j: int) -> bool:
-        # TODO
-        return True
+    def to_numpy(v: Vector3) -> np.ndarray:
+        return np.array([v.x, v.y, v.z])
