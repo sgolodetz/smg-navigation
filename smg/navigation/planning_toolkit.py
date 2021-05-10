3b945cb913f09a0b89e85e8eb8474660021589f0
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-05-10 21:49:09 +0100

    Continue adding comments.

--- smg/navigation/planning_toolkit.py
+++ smg/navigation/planning_toolkit.py
@@ -44,6 +44,13 @@ class PlanningToolkit:
 
     @staticmethod
     def interpolate_path(path: np.ndarray, *, new_length: int = 100) -> np.ndarray:
+        """
+        Make a smoother version of a path by using curve fitting and interpolation.
+
+        :param path:        The original path.
+        :param new_length:  The number of points to take from the interpolating curve.
+        :return:            The interpolated path.
+        """
         x: np.ndarray = np.arange(len(path))
         cs: Akima1DInterpolator = Akima1DInterpolator(x, path)
         return cs(np.linspace(0, len(path) - 1, new_length))
@@ -185,13 +192,25 @@ class PlanningToolkit:
     # PUBLIC METHODS
 
     def chord_is_traversible(self, path: np.ndarray, source: int, dest: int, *, use_clearance: bool) -> bool:
+        """
+        Check whether the specified (straight line) chord between two points on a path can be directly traversed.
+
+        :param path:            The path.
+        :param source:          The index of the source point on the path.
+        :param dest:            The index of the destination point on the path.
+        :param use_clearance:   Whether to require there to be sufficient "clearance" around the chord.
+        :return:                True, if the chord is traversible, or False otherwise.
+        """
+        # Look up the voxels containing the source and destination points.
         source_node: PathNode = self.pos_to_node(path[source, :])
         dest_node: PathNode = self.pos_to_node(path[dest, :])
 
         source_vpos: np.ndarray = self.node_to_vpos(source_node)
         dest_vpos: np.ndarray = self.node_to_vpos(dest_node)
 
-        # TODO: Fix and optimise this.
+        # Test voxels along the chord for their traversibility. If any of them is non-traversible, so is the chord.
+        # TODO: Fix and optimise this. It can fail if the chord's very long (through not testing enough points),
+        #       and it's needlessly slow. A midpoint line algorithm should be used instead.
         prev_node: Optional[PathNode] = None
         for t in np.linspace(0.0, 1.0, 101):
             pos: np.ndarray = source_vpos * (1 - t) + dest_vpos * t
@@ -239,6 +258,12 @@ class PlanningToolkit:
         return np.array([node[i] * voxel_size + half_voxel_size for i in range(3)])
 
     def occupancy_status(self, node: PathNode) -> str:
+        """
+        Get the occupancy status of the Octomap voxel corresponding to the specified path node.
+
+        :param node:    A path node.
+        :return:        The occupancy status of the Octomap voxel corresponding to the path node.
+        """
         # FIXME: Use an enumeration for the return values.
         vpos: np.ndarray = self.node_to_vpos(node)
         octree_node: Optional[OcTreeNode] = self.__tree.search(Vector3(*vpos))
@@ -259,6 +284,13 @@ class PlanningToolkit:
         return tuple(np.round(pos // voxel_size).astype(int))
 
     def pull_strings(self, path: np.ndarray, *, use_clearance: bool) -> np.ndarray:
+        """
+        TODO
+
+        :param path:            TODO
+        :param use_clearance:   TODO
+        :return:                TODO
+        """
         pulled_path: List[np.ndarray] = []
 
         i: int = 0
