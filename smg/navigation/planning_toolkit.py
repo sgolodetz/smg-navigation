6fa5250f6bdaafff43b54eee9088ec9839dbf24f
Stuart Golodetz <sgolodetz@gxstudios.net>
2022-05-06 14:04:44 +0100

    Introduce PlanningToolkit.find_flat_ground_below.

--- smg/navigation/planning_toolkit.py
+++ smg/navigation/planning_toolkit.py
@@ -227,6 +227,39 @@ class PlanningToolkit:
         dest_vpos: np.ndarray = self.node_to_vpos(dest_node)
         return self.line_segment_is_traversable(source_vpos, dest_vpos, use_clearance=use_clearance)
 
+    def find_flat_ground_below(self, pos: np.ndarray) -> Optional[np.ndarray]:
+        """
+        Try to find a patch of flat ground below the specified position.
+
+        :param pos: The specified position.
+        :return:    The centre of the voxel in the middle of the patch (if found), or None otherwise.
+        """
+        # Perform a downwards search from the specified position to find the ground.
+        resolution: float = self.__tree.get_resolution()
+
+        test_vpos: np.ndarray = self.pos_to_vpos(pos)
+        test_node: PathNode = self.pos_to_node(test_vpos)
+
+        # Step downwards a voxel at a time until the node being tested is non-traversable.
+        while self.node_is_traversable(
+            test_node, neighbours=PlanningToolkit.neighbours8, use_clearance=True
+        ):
+            # If we've stepped outside the bounds of the octree, early out.
+            if not self.is_in_bounds(test_vpos):
+                return None
+
+            # Otherwise, keep stepping downwards.
+            test_vpos[1] += resolution
+            test_node = self.pos_to_node(test_vpos)
+
+        # Once we've found the ground, check that the surrounding patch is sufficiently flat. If it isn't, return None.
+        for neighbour_node in PlanningToolkit.neighbours8(test_node):
+            if self.node_is_free(neighbour_node):
+                return None
+
+        # If the patch is flat enough, return the centre of its middle voxel.
+        return test_vpos
+
     def get_tree(self) -> OcTree:
         """
         Get the octree associated with the toolkit.
